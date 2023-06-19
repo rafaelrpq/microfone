a80e61cc0b3658c35bbb54b1691b420090808ff7
Rafael Quevedo <rafaelrpq@gmail.com>
2023-06-19 17:45:14 -0300

    pwa

---
+++ src/audioControl.js
@@ -0,0 +1,70 @@
+let audioContext;
+let audioStream;
+let scriptNode;
+
+let playback = false;
+let img = document.querySelector ('img');
+let label = document.querySelector ('label')
+
+
+const startButton = document.getElementById ('startButton');
+const audioPlayer = document.getElementById ('audioPlayer');
+
+startButton.addEventListener ('click', playbackControl);
+startButton.addEventListener ('contextmenu', (e) => {
+        e.preventDefault()
+        e.stopPropagation()
+        e.stopImmediatePropagation();
+        return false;
+});
+
+function startPlayback () {
+    navigator.mediaDevices.getUserMedia ({ audio: true })
+    .then (function (stream) {
+        audioStream = stream;
+        audioContext = new AudioContext ();
+        const input = audioContext.createMediaStreamSource (stream);
+
+        const bufferSize = 2048;
+        scriptNode = audioContext.createScriptProcessor (bufferSize, 1, 1);
+
+        scriptNode.onaudioprocess = function  (audioProcessingEvent) {
+            const inputBuffer = audioProcessingEvent.inputBuffer;
+            const outputBuffer = audioProcessingEvent.outputBuffer;
+
+            for (let channel = 0; channel < outputBuffer.numberOfChannels; channel++) {
+                const inputData = inputBuffer.getChannelData (channel);
+                const outputData = outputBuffer.getChannelData (channel);
+
+                for (let i = 0; i < inputBuffer.length; i++) {
+                    outputData[i] = inputData[i];
+                }
+            }
+        };
+
+        input.connect (scriptNode);
+        scriptNode.connect (audioContext.destination);
+    })
+    .catch  (function (err) {
+        console.log ('Ocorreu um erro ao acessar o microfone: ' + err);
+        alert ('Ocorreu um erro ao acessar o microfone: ' + err);
+    });
+}
+
+function stopPlayback () {
+    audioStream.getTracks ().forEach (function (track) {
+        track.stop ();
+    });
+    audioContext.close ();
+    scriptNode.disconnect ();
+    startButton.disabled = false;
+}
+
+function playbackControl () {
+    navigator.vibrate (50)
+    playback = !playback;
+    img.src = (playback) ? 'res/microphone-slash-duotone.svg' : 'res/microphone-duotone.svg'
+    label.innerHTML = (playback) ? 'Parar Reprodução' : 'Iniciar Reprodução'
+    startButton.className = (playback) ? 'playing' : ''
+    return (playback) ? startPlayback () : stopPlayback  ()
+}
